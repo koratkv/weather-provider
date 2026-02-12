80ed9fc0e382fff8147cdbe54d84dbc068cf1f7c
Pramod Magesh <pramodmagesh20@gmail.com>
2026-02-12 09:36:52 +0530

    CORS optios updated

--- backend/index.js
+++ backend/index.js
@@ -7,7 +7,7 @@ const app = express();
 const cors = require("cors");
 
 const corsOptions = {
-    origin: process.env.CLIENT_URL || "http://localhost:5173",
+    origin: process.env.CLIENT_URL,
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
